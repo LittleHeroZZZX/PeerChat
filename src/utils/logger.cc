49a1ffee444a6ffc24fdf882b78e1b005306cf30
zhouxin <zhou.xin2000@outlook.com>
2024-11-22 18:22:26 +0800

    添加日志辅助打印函数

--- src/utils/logger.cc
+++ src/utils/logger.cc
@@ -3,3 +3,19 @@
 // 静态成员初始化
 std::shared_ptr<spdlog::logger> Logger::instance = nullptr;
 std::once_flag Logger::init_instance_flag;
+
+void log_error(const std::string &message) {
+    Logger::get_instance()->error(message);
+}
+
+void log_info(const std::string &message) {
+    Logger::get_instance()->info(message);
+}
+
+void log_debug(const std::string &message) {
+    Logger::get_instance()->debug(message);
+}
+
+void log_warning(const std::string &message) {
+    Logger::get_instance()->warn(message);
+}
